0060767180c292838e69c1c9ef232de8ef1f1300
Faraj Farook <farajfarook@gmail.com>
2022-06-24 15:52:23 +1000

    Adding zustand for sync state management

--- src/applications-module/store/applicationsStoreSwr.ts
+++ src/applications-module/store/applicationsStoreSwr.ts
@@ -3,15 +3,39 @@ import { FilterConditions } from "../../shared-module";
 import { Application } from "../models/application";
 import { getApplications } from "../services/applicationsService";
 import { ApplicationsState } from "./applicationsStore";
+import create from 'zustand'
 
 const _cacheKey = (condition: FilterConditions): string => `applications-${JSON.stringify(condition)}`
 
-export const useApplicationsStoreSwr = (condition: FilterConditions): ApplicationsState => {
+export const useApplicationsStoreSwr = (condition: FilterConditions): ApplicationsState & {
+    //selectedApplicationId: string | null,
+    //selectedApplication: Application | null
+} => {
 
-    const { data, isValidating } = useSWR<Application[]>(_cacheKey(condition), () => getApplications(condition))
+    const { data, isValidating } = useSWR<Application[]>(_cacheKey(condition),
+        () => getApplications(condition))
+
+    //const { selectedApplicationId } = useApplicationsIdStore()
+
+    //const { selectedApplication} = data?.find(app => app.id === selectedApplicationId)
 
     return ({
         applications: data || [],
-        isLoading: isValidating
+        isLoading: isValidating,
+        //selectedApplicationId,
+        //selectedApplication
     })
 }
+
+interface ApplicationIdState {
+    selectedApplicationId: string | null
+    select: (applicationId: string) => void
+}
+
+export const useApplicationsIdStoreSync = create<ApplicationIdState>()((set, get) => ({
+    selectedApplicationId: null,
+
+    select: (applicationId: string) => {
+        set(state => ({ ...state, selectedApplicationId: applicationId }))
+    }
+}))
