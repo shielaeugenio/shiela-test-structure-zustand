2f34f53adaffc9a0650de94b920b385754725f6b
shielae <shielae@pageuppeople.com>
2022-07-08 12:15:37 +0800

    clean master

--- src/filters-module/index.ts
+++
@@ -1,9 +0,0 @@
-import FilterComponent from "./components/FilterComponent";
-import { filterSlice, FilterSlice } from "./state/filterSlice";
-
-export {
-    filterSlice,
-    FilterComponent
-};
-export type { FilterSlice };
-
